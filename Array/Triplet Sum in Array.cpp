7211f4310df6395aafb736b2da376725e032c4b9
Gautam Tomar <116489929+7matuag@users.noreply.github.com>
2023-01-06 21:56:44 +0530

    Create Triplet Sum in Array.cpp

---
+++ Array/Triplet Sum in Array.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    bool find3Numbers(int A[], int n, int X)
+    {
+        //Your Code Here
+        sort(A,A+n);
+        for(int i=0;i<n;i++){
+            int j=i+1,k=n-1;
+            while(k>j){
+                if(A[i]+A[j]+A[k]==X){
+                    return 1;
+                }
+                else if(A[i]+A[j]+A[k]>X){
+                    k--;
+                }
+                else{
+                    j++;
+                }
+            }
+        }
+        return 0;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		int n,X;
+		cin>>n>>X;
+		int i,A[n];
+		for(i=0;i<n;i++)
+			cin>>A[i];
+		Solution ob;
+        cout <<  ob.find3Numbers(A, n, X) << endl;
+    }
+}
+
+// } Driver Code Ends
