e12b1f9ec5c6a2099f19e3bdd5b10118f41712a8
Gautam Tomar <116489929+7matuag@users.noreply.github.com>
2023-01-08 13:38:19 +0530

    Create Count Squares.cpp

---
+++ Search & Sort/Count Squares.cpp
@@ -0,0 +1,32 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int countSquares(int N) {
+        // code here
+        int i=1,k=0;
+        while(i*i<N){
+            k++;
+            i++;
+        }
+        return k;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.countSquares(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
