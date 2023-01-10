62f03b35f969200e38e1ef20cf5e733eb810880e
Gautam Tomar <116489929+7matuag@users.noreply.github.com>
2023-01-10 19:05:13 +0530

    Create Count triplets with sum smaller than X.cpp

---
+++ Search & Sort/Count triplets with sum smaller than X.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	long long countTriplets(long long arr[], int n, long long sum)
+	{
+	    // Your code goes here
+	    long long int ans=0;
+	    sort(arr,arr+n);
+	    for(int i=0;i<n-2;i++){
+	        long long int s=arr[i];
+	        int j=i+1,k=n-1;
+	        while(j<k){
+	            if(arr[i]+arr[j]+arr[k]<sum){
+	                ans+=(k-j);
+	                j++;
+	            }
+	            else{
+	                k--;
+	            }
+	        }
+	    }
+	    return ans;
+	}
+		 
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+  
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+    	long long sum;
+        cin>>n>>sum;
+        long long arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+	    
+
+       
+
+        Solution ob;
+        cout << ob.countTriplets(arr, n, sum) ;
+	   
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
