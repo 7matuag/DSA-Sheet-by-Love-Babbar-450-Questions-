9049d5ab7ec5a13103d0b4ae1f1c32634d834b0b
Gautam Tomar <116489929+7matuag@users.noreply.github.com>
2023-01-15 13:59:54 +0530

    Create Find position of set bit.cpp

---
+++ Bit Manipulation/Find position of set bit.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findPosition(int N) {
+        // code here
+        if(log2(N)==(int)log2(N)){
+            return log2(N)+1;
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+
+        cin>>N;
+
+        Solution ob;
+        cout << ob.findPosition(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
