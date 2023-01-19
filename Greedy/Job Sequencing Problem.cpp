f76bcf9986cdef0d27760fa364f27c6dc3620469
Gautam Tomar <116489929+7matuag@users.noreply.github.com>
2023-01-20 00:04:04 +0530

    Create Job Sequencing Problem.cpp

---
+++ Greedy/Job Sequencing Problem.cpp
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+// Program to find the maximum profit job sequence from a given array 
+// of jobs with deadlines and profits 
+#include<bits/stdc++.h>
+using namespace std; 
+
+// A structure to represent a job 
+struct Job 
+{ 
+    int id;	 // Job Id 
+    int dead; // Deadline of job 
+    int profit; // Profit if job is over before or on deadline 
+}; 
+
+
+// } Driver Code Ends
+/*
+struct Job 
+{ 
+    int id;	 // Job Id 
+    int dead; // Deadline of job 
+    int profit; // Profit if job is over before or on deadline 
+};
+*/
+
+class Solution 
+{
+    public:
+    //Function to find the maximum profit and the number of jobs done.
+    static bool cmp(Job &a,Job &b){
+        return b.profit<a.profit;
+    }
+    vector<int> JobScheduling(Job arr[], int n) 
+    {
+        sort(arr,arr+n,cmp);
+        int prof=0,cnt=0;
+        vector<bool>visited(n,0);
+        for(int i=0;i<n;i++){
+            int j=arr[i].dead;
+            while(j-1>=0 && visited[j-1]){
+                j--;
+            }
+            if(j-1>=0 && !visited[j-1]){
+                visited[j-1]=1;
+                prof+=arr[i].profit;
+                cnt++;
+            }
+        }
+        return {cnt,prof};
+    } 
+};
+
+//{ Driver Code Starts.
+// Driver program to test methods 
+int main() 
+{ 
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        Job arr[n];
+        
+        //adding id, deadline, profit
+        for(int i = 0;i<n;i++){
+                int x, y, z;
+                cin >> x >> y >> z;
+                arr[i].id = x;
+                arr[i].dead = y;
+                arr[i].profit = z;
+        }
+        Solution ob;
+        //function call
+        vector<int> ans = ob.JobScheduling(arr, n);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+	return 0; 
+}
+
+
+
+// } Driver Code Ends
