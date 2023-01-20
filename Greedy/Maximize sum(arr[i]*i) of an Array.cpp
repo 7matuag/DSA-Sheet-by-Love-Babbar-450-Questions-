75aac4338be00f4b9de935444874f8aa5c42919d
Gautam Tomar <116489929+7matuag@users.noreply.github.com>
2023-01-20 20:47:32 +0530

    Create Maximize sum(arr[i]*i) of an Array.cpp

---
+++ Greedy/Maximize sum(arr[i]*i) of an Array.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int Maximize(int a[],int n)
+    {
+        // Complete the function
+        long long int sum=0,mod=1e9+7;
+        sort(a,a+n);
+        for(long long int i=0;i<n;i++){
+            sum+=(i*a[i])%mod;
+        }
+        return sum%mod;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        cout<<ob.Maximize(a,n)<<endl;
+    }
+}
+// } Driver Code Ends
