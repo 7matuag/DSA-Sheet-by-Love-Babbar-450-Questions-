9ecbf54eac02d2859c9b7e92e0bd23454195c5da
Gautam Tomar <116489929+7matuag@users.noreply.github.com>
2023-02-13 22:03:27 +0530

    Create The Celebrity Problem.cpp

---
+++ Stacks & Queues/The Celebrity Problem.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution 
+{
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        // code here
+        int cel=0;
+        for(int i=0;i<n;i++){
+            if(M[cel][i]==1){
+                cel=i;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(i!=cel && (M[i][cel]==0 || M[cel][i]==1)){
+                return -1;
+            }
+        }
+        return cel;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > M( n , vector<int> (n, 0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cin>>M[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.celebrity(M,n)<<endl;
+
+    }
+}
+
+// } Driver Code Ends
