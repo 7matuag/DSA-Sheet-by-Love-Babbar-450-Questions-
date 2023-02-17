84df6cec4e1b0139bc80c84c83cc61d7dc7d073f
Gautam Tomar <116489929+7matuag@users.noreply.github.com>
2023-02-17 17:49:24 +0530

    Create Is Binary Tree Heap.cpp

---
+++ Heap/Is Binary Tree Heap.cpp
@@ -0,0 +1,150 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+// User Function template for C++
+
+// Structure of node
+/*struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+    bool isCompleteBT(Node* root){
+        //code here
+        queue<Node*>q;
+        q.push(root);
+        while(!q.empty()){
+            if(q.front()){
+                q.push(q.front()->left);
+                q.push(q.front()->right);
+                q.pop();
+            }
+            else{
+                break;
+            }
+        }
+        while(!q.empty()){
+            if(q.front()){
+                return 0;
+            }
+            q.pop();
+        }
+        return 1;
+    }
+    bool solve(struct Node* tree,int x){
+        if(!tree){
+            return 1;
+        }
+        if(tree->data>x){
+            return 0;
+        }
+        return solve(tree->left,tree->data) && solve(tree->right,tree->data);
+    }
+    bool isHeap(struct Node* tree) {
+        // code here
+        return solve(tree,INT_MAX) && isCompleteBT(tree);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Solution ob;
+        Node *root = buildTree(treeString);
+        if (ob.isHeap(root))
+            cout << 1 << endl;
+        else
+            cout << 0 << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
