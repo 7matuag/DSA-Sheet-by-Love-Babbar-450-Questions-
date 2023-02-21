d89f0e3ece52e3a56f37741e3cbe04848c9adc05
Gautam Tomar <116489929+7matuag@users.noreply.github.com>
2023-02-21 17:55:03 +0530

    Create Check if it is possible to survive on Island.cpp

---
+++ Greedy/Check if it is possible to survive on Island.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int minimumDays(int S, int N, int M){
+        // code here
+        int a=M*S;
+        int b=S-S/7;
+        int c=N*b;
+        if(a>c){
+            return -1;
+        }
+        return ceil(a/(N*1.0));
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int S, N, M;
+        cin>> S >> N >> M;
+        
+        Solution ob;
+        cout<<ob.minimumDays(S, N, M)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
