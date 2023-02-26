ee0c30a7d4fdc5cb989a12a10aab2bdf03b4b865
Gautam Tomar <116489929+7matuag@users.noreply.github.com>
2023-02-26 11:03:53 +0530

    Create Insert An Element At Its Bottom In A Given Stack.cpp

---
+++ Stacks & Queues/Insert An Element At Its Bottom In A Given Stack.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+void solve(stack<int>&s,int x){
+    if(s.empty()){
+        s.push(x);
+        return ;
+    }
+    int temp=s.top();
+    s.pop();
+    solve(s,x);
+    s.push(temp);
+}
+stack<int> pushAtBottom(stack<int>& myStack, int x) 
+{
+    // Write your code here.
+    solve(myStack,x);
+    return myStack;
+}
