be6561119f34ea2c0861b3bab0b18ba8c5b90d3c
Gautam Tomar <116489929+7matuag@users.noreply.github.com>
2023-02-27 08:19:06 +0530

    Create Circular tour.cpp

---
+++ Stacks & Queues/Circular tour.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};
+
+
+// } Driver Code Ends
+/*
+The structure of petrolPump is 
+struct petrolPump
+{
+    int petrol;
+    int distance;
+};*/
+
+/*You are required to complete this method*/
+class Solution{
+  public:
+  
+    //Function to find starting point where the truck can start to get through
+    //the complete circle without exhausting its petrol in between.
+    int tour(petrolPump p[],int n)
+    {
+       //Your code here
+       int totalpetrol=0;
+        int ptrl=0,ans=0;
+        for(int i=0;i<n;i++){
+            ptrl+=(p[i].petrol-p[i].distance);
+            totalpetrol+=(p[i].petrol-p[i].distance);
+            if(ptrl<0){
+                ptrl=0;
+                ans=i+1;
+            }
+        }
+        if(totalpetrol>=0){
+            return ans;
+        }
+        return -1;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        petrolPump p[n];
+        for(int i=0;i<n;i++)
+            cin>>p[i].petrol>>p[i].distance;
+        Solution obj;
+        cout<<obj.tour(p,n)<<endl;
+    }
+}
+
+// } Driver Code Ends
